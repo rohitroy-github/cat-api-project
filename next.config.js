c030192bd1385468edcbea9165e36711a0efae31
rohit <rhtry.tech@gmail.com>
2022-11-09 03:32:24 +0530

    Adding files ...!

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["cdn2.thecatapi.com", "26.media.tumblr.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
